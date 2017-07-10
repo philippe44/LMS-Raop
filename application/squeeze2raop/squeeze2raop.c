cc196504fecd4f7dc45bf4f12a7f6c9a323b655a
philippe44 <philippe_44@outlook.com>
2017-07-09 18:21:42 -0700

    0.3.0.0.5

--- application/squeeze2raop/squeeze2raop.c
+++ application/squeeze2raop/squeeze2raop.c
@@ -306,7 +306,7 @@ void 		DelRaopDevice(struct sMR *Device);
 			// first convert to 0..100 value
 			for (LMSVolume = 100; Volume < LMSVolumeMap[LMSVolume] && LMSVolume; LMSVolume--);
 
-			if (device->Config.VolumeMode == VOLUME_HARD &&	gettime_ms() - device->VolumeStamp > 1000 &&
+			if (device->Config.VolumeMode == VOLUME_HARD &&	(u64_t) gettime_ms() > (u64_t) device->VolumeStamp + 1000 &&
 				(Volume || device->Config.MuteOnPause || sq_get_mode(device->SqueezeHandle) == device->sqState)) {
 				tRaopReq *Req = malloc(sizeof(tRaopReq));
 
@@ -377,7 +377,7 @@ static void *PlayerThread(void *args)
 
 			LOG_DEBUG("[%p]: tick %u", Device, now);
 
-			if (Device->DiscWait && now > Device->LastFlush + Device->Config.IdleTimeout * 1000) {
+			if (Device->DiscWait && (u64_t) now > (u64_t) Device->LastFlush + Device->Config.IdleTimeout * 1000) {
 				Device->VolumeReady = !Device->Config.VolumeTrigger;
 				LOG_INFO("[%p]: Disconnecting %u", Device, now);
 				raopcl_disconnect(Device->Raop);
