83eb1e846129e5464ab0bd57aac172744b962fe4
philippe44 <philippe_44@outlook.com>
2017-08-10 20:24:35 -0700

    0.3.1.0.2

--- application/squeeze2raop/squeeze2raop.c
+++ application/squeeze2raop/squeeze2raop.c
@@ -1161,6 +1161,8 @@ static bool Start(void)
 	glHost.s_addr = get_localhost(&glHostName);
 	if (!strstr(glInterface, "?")) glHost.s_addr = inet_addr(glInterface);
 
+	LOG_INFO("Binding to %s", inet_ntoa(glHost));
+
 	// init mDNS
 	gl_mDNSId = init_mDNS(false, glHost);
 
